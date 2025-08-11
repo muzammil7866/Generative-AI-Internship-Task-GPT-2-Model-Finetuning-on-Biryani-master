8b139feb1c597a5be023281486a7a70190049c2b
Muzammil Bin Sohail <160920532+muzammil7866@users.noreply.github.com>
2025-08-12 01:54:53 +0500

    Update generate.py

--- generate.py
+++ generate.py
@@ -3,4 +3,5 @@ from transformers import pipeline
 generator = pipeline("text-generation", model="./gpt2-finetuned")
 prompt = "Biryani is very"
 output = generator(prompt, max_length=50, num_return_sequences=1)
+print("Response after Model Finetuning: \n\n")
 print(output[0]['generated_text'])
